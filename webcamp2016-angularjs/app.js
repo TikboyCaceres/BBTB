627f53c20d0cd8f691f3c2ee940021ae56c5c0c1
TikboyCaceres <TikboyCaceres@gmail.com>
2016-07-17 13:13:11 +0800

    Add files via upload

--- webcamp2016-angularjs/app.js
+++ webcamp2016-angularjs/app.js
@@ -1,11 +1,13 @@
 angular.module('orderByExample3', [])
 .controller('ExampleController', ['$scope', 'orderByFilter', function($scope, orderBy) {
   var friends = [
-    {name: 'Column 1',   phone: '1',  age: 10},
-    {name: 'Column 2',   phone: '2',  age: 19},
-    {name: 'Column 3',   phone: '3',  age: 21},
-    {name: 'Column 4',   phone: '4',  age: 35},
-    {name: 'Column 5',  phone: '5',  age: 29}
+    {name: 'A',   phone: '3',  age: 10},
+    {name: 'C',   phone: '4',  age: 19},
+    {name: 'W',   phone: '6',  age: 21},
+    {name: 'Z',   phone: '2',  age: 35},
+    {name: 'Z',   phone: '2',  age: 35},
+    {name: 'Z',   phone: '2',  age: 35},
+    {name: 'b',  phone: '1',  age: 29}
   ];
 
   $scope.propertyName = 'age';
--- webcamp2016-angularjs/sample.js
+++ webcamp2016-angularjs/sample.js
@@ -1,11 +1,11 @@
 angular.module('orderByExample3', [])
 .controller('ExampleController', ['$scope', 'orderByFilter', function($scope, orderBy) {
   var friends = [
-    {name: 'John',   phone: '555-1212',  age: 10},
-    {name: 'Mary',   phone: '555-9876',  age: 19},
-    {name: 'Mike',   phone: '555-4321',  age: 21},
-    {name: 'Adam',   phone: '555-5678',  age: 35},
-    {name: 'Julie',  phone: '555-8765',  age: 29}
+    {name: 'John',   phone: '555-1212',  age: 1},
+    {name: 'Mary',   phone: '555-9876',  age: 7},
+    {name: 'Mike',   phone: '555-4321',  age: 5},
+    {name: 'Adam',   phone: '555-5678',  age: 10},
+    {name: 'Julie',  phone: '555-8765',  age: 0}
   ];
 
   $scope.propertyName = 'age';
